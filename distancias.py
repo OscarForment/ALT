7c577a160d2ea03794cc72bdeef13c84b86ae775
OscarForment <oscarforment@gmail.com>
2023-09-29 09:04:51 +0200

    levenshtein_edicion completada

--- distancias.py
+++ distancias.py
@@ -19,7 +19,47 @@ def levenshtein_matriz(x, y, threshold=None):
 
 def levenshtein_edicion(x, y, threshold=None):
     # a partir de la versión levenshtein_matriz
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=int)
+    for i in range(1, lenX + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenX + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+            )
+    #Ahora tenemos la matriz en D
+    camino=[]
+    i=lenX
+    j=lenY
+    while i>0 and j>0: #Recorremos la matriz desde el final
+        min_move=min(
+                D[i - 1][j],
+                D[i][j - 1],
+                D[i - 1][j - 1],
+            )
+        if D[i-1][j]==min_move and D[i][j]==D[i-1][j]+1: #comprobamos si es borrado teniendo en cuenta que siempre tiene coste 1
+            camino.append((x[i-1],""))
+            i-=1
+        elif D[i][j-1]==min_move and D[i][j]==D[i][j-1]+1: #comprobamos si es inserción teniendo en cuenta que siempre tiene coste 1
+            camino.append(("",y[j-1]))
+            j-=1
+        else: #sino pues es sustición
+            camino.append((x[i-1],y[j-1]))
+            i-=1
+            j-=1
+    while i>0: #por si solo quedan operaciones de borrado
+        camino.append((x[i-1],""))
+        i-=1
+    while j>0: #por si solo quedan operaciones de inserción
+        camino.append(("",y[j-1]))
+        j-=1
+       
+    camino.reverse()
+    return D[lenX, lenY],camino
 
 def levenshtein_reduccion(x, y, threshold=None):
     # completar versión con reducción coste espacial
