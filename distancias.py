44ad5138d53bb4dd98f03ac7593eb31d518ae68b
Manuel Sarmiento Tendero <MSARTEN@upv.edu.es>
2023-11-08 20:44:51 +0100

    damerau restricted edicion

--- distancias.py
+++ distancias.py
@@ -165,7 +165,7 @@ def damerau_restricted_edicion(x, y, threshold=None):
                 D[i - 1][j],
                 D[i][j - 1],
                 D[i - 1][j - 1],
-                D[i - 2][j - 2],
+                D[i - 2][j - 2]
             )
         if D[i-1][j]==min_move and D[i][j]==D[i-1][j]+1: #comprobamos si es borrado teniendo en cuenta que siempre tiene coste 1
             camino.append((x[i-1],""))
@@ -173,8 +173,8 @@ def damerau_restricted_edicion(x, y, threshold=None):
         elif D[i][j-1]==min_move and D[i][j]==D[i][j-1]+1: #comprobamos si es inserción teniendo en cuenta que siempre tiene coste 1
             camino.append(("",y[j-1]))
             j-=1
-        elif D[i-2][j-2]==min_move and D[i][j]==D[i-2][j-2]+1: #
-            camino.append((x[i-2],y[j-2]))
+        elif D[i-2][j-2]==min_move and D[i][j]==D[i-2][j-2]+1: #Comprobamos si es una transposición teniendo en cuenta que tiene coste 1
+            camino.append((x[i-2]+x[i-1],y[j-1]+y[i-2]))
             i-=2
             j-=2
         else:#sino pues es sustición
